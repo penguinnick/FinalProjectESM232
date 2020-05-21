fc327ec3b64818eea63a5dbc1ab9d9ab1bde0e2f
gorchels <gorchels@bren.ucsb.edu>
2020-05-20 22:04:01 -0700

    yield function started

---
+++ R/wheat_yield.R
@@ -0,0 +1,8 @@
+#' @param m_T is the mean Temperature in main growing month (C) for each year of interest
+#' @param mm_P mm of rain in main growing month
+
+wheat_yield = function()
+# http://www.fao.org/3/y4011e04.htm
+  #opitimal wheat temperature is 25 C
+  #1 tonne/ha is average yield in Egypt with rain fed wheat
+  # 1.42 tonne/ha is average barley yield 
