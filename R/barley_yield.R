b701540c1535544564b763015e5930060317a9f4
gorchels <gorchels@bren.ucsb.edu>
2020-05-20 22:34:10 -0700

    Basic yield functions generated

--- R/barley_yield.R
+++ R/barley_yield.R
@@ -1,14 +1,13 @@
 #' @param  yields historic yield data frame provide at least 30 years of data
 #' as annual (year, mean_yield (tonnes/ha))
 #' Assumes data is normally distributed! Graph data beforehand
-wheat_yield = function(yields) {
-  wheat=yields
+barley_yield = function(yields) {
+  barley=yields
   
-  mean_yield = mean(wheat$mean_yield)
-  sd_yield = sd(wheat$mean_yield)
+  mean_yield = mean(barley$mean_yield)
+  sd_yield = sd(barley$mean_yield)
   
   yield = rnorm(1,mean=mean_yield, sd=sd_yield)
-
   if(yield <=0)
   {
     return(0)
