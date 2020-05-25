3a1c5231c7eaf8a3afa32f256464f4583c5a2632
gorchels <gorchels@bren.ucsb.edu>
2020-05-25 11:28:36 -0700

    Updated yield functions

--- R/wheat_yield.R
+++ R/wheat_yield.R
@@ -1,13 +1,17 @@
 #' @param  yields historic yield data frame provide at least 30 years of data
 #' as annual (year, mean_yield (tonnes/ha))
 #' Assumes data is normally distributed! Graph data beforehand
-wheat_yield = function(yields) {
+#' @param area is the area of the subsistance farm used for wheat in hectares
+#
+#' 
+#' 
+wheat_yield = function(yields, area=1) {
   wheat=yields
   
   mean_yield = mean(wheat$mean_yield)
   sd_yield = sd(wheat$mean_yield)
   
-  yield = rnorm(1,mean=mean_yield, sd=sd_yield)
+  yield = area*rnorm(1,mean=mean_yield, sd=sd_yield)
 
   if(yield <=0)
   {
