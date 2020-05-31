021033cd03179706857baab24c96aa52ddcd0099
penguinnick <34468253+penguinnick@users.noreply.github.com>
2020-05-31 14:08:34 -0700

    sdp_farm function cleaned but needs debugging final rmd updated

---
+++ R/sdp_farm.R
@@ -0,0 +1,68 @@
+#' @param x_class sequence of wealth classes from 0 to maximum wealth
+#' @param PrS probabilities of good and bad year affecting livestock strategy choice
+#' @parms a list with model parameters
+#' @cropparms a list summarizing yield per hectare for crops considered
+#' @stockparms a list containing arguments passed to evolve_pop_wofftake.R
+
+
+source("./R/wealth.R")
+source("./R/wheat_yield.R")
+source("./R/barley_yield.R")
+
+sdp_farm <- function(f,s,parms, crop.parms, stock.parms, x_class, times, max_store=20, PrS=c(0.85, 0.65)){
+  # x_max=parms$x_max
+  # x_crit=parms$x_crit
+  with(as.list(c(crop.parms, stock.parms, parms)),{
+    
+    # # initialize outputs
+    # times=seq(1:t_max)
+    # nclass=length(x_class)
+    # f <- matrix(nrow=t_max,ncol=nclass ,0) # optimal fitness
+    # s <- matrix(nrow=t_max,ncol=nclass ,0) # surplus
+    # best.strategy <- matrix(nrow=t_max-1,ncol=nclass-1,0) # best patch
+    f[t_max,] <- hsr
+    s[t_max,] <- max_store
+    V <- vector(length = sum(ncrops,nstocks))
+    
+    for(t in rev(times)){
+      for (x in x_class[-1]){
+        # crops
+        wheatgain=wheat_yield(yields = wheatY)
+        barleygain=barley_yield(yields = barleyY)
+        cropgains <- c(wheatgain, barleygain) # variable crop gain 
+        cropcost <- c(cropgains/10) # crop cost is proportional to yield
+        p.crops <- c(ppois(wheatgain, lambda = wheatgain), ppois(barleygain, lambda = barleygain))
+        expectedcropgain <- p.crops*(wealth(x-cropcost+cropgains,t+1) + (1-p.crops)*(wealth(x-cropcost+cropgains,t+1)))
+        
+        
+        # livestock
+        livestock <- lapply(offtake, function(o){tmp=evolve_pop_wofftake(fertility, survivability, initialpop = p0, nstep = 3, offtake=o); return(tmp)})
+        G.stock.products <- as.vector(unlist(lapply(offtake, function(o){tmp=evolve_pop_wofftake(fertility, survivability, initialpop = p0, nstep = 3, offtake=o); return(sum(tmp$offtakes))})))
+        G.new.stock <- as.vector(unlist(lapply(offtake, function(o){tmp=evolve_pop_wofftake(fertility, survivability, initialpop = p0, nstep = 3, offtake=o); return(tmp$newstock[3])})))
+        B.stock.products <- as.vector(unlist(lapply(offtake, function(o){tmp=evolve_pop_wofftake(fertility, (1-survivability), initialpop = p0, nstep = 3, offtake=o); return(sum(tmp$offtakes))})))
+        B.new.stock <- as.vector(unlist(lapply(offtake, function(o){tmp=evolve_pop_wofftake(fertility, (1-survivability), initialpop = p0, nstep = 3, offtake=o); return(tmp$newstock[3])})))
+        newp <- as.vector(lapply(offtake, function(o){tmp=evolve_pop_wofftake(fertility, survivability, initialpop = p0, nstep = 3, offtake=o); return(tmp$popbyage[,2])}))
+        oldp <- as.vector(lapply(offtake, function(o){tmp=evolve_pop_wofftake(fertility, survivability, initialpop = p0, nstep = 3, offtake=o); return(tmp$popbyage[,1])}))
+        current.herd <- as.vector(unlist(lapply(oldp, function(n){sum(n)})))
+        expectedstockgain <- PrS*((wealth(x+current.herd-G.stock.products,t+1)) + (wealth(x+current.herd-B.stock.products,t+1)))
+        
+        # compare all gains
+        gains <- c(expectedcropgain[1]+expectedstockgain[1], expectedcropgain[1]+expectedstockgain[2], expectedcropgain[2]+expectedstockgain[1], expectedcropgain[2]+expectedstockgain[2])
+        V <- gains-hsr
+        Vs <- ifelse(V<0, V+(s[t-1,x]*0.95), V)
+        Vs[Vs<0] <- 0 # destitute
+        f[t,x-x_crit+1] <- max(V) # optimal fitness
+        # best strategy at previous timestep
+        best <- which.max(Vs)
+        beststrategy[t-1,x] <- best
+        # add to surplus
+        s[t,x] <- ifelse(Vs[best]-hsr>0,Vs[best]-hsr,0)
+        s[t,x] <- ifelse(s[t,x]>max_store, max_store, s[t,x])
+        # reset goat herd size
+        p0 <- ifelse(which.max(V)%%2==0, as.vector(newp[[2]]), as.vector(newp[[1]]))
+      }
+    }
+  })
+  return(list(FarmWealth=f, Surplus=s, OptimalStrategy=best.strategy))
+}
+
---
+++ R/wealth.R
@@ -0,0 +1,22 @@
+#' wealth function performs appropriate cut-offs as specified in parms. for use in sdp_farm function 
+#' @param  x vector length of possible outcomes
+#' @param  t the current time step
+#' @parms  parms as specified for use of sdp_farm function
+#' returns vector of length x
+
+
+
+wealth <- function(x,t, parms=parms)
+{
+  xx <- pmin(x ,x_max)
+  xx <- pmax(xx,x_crit)
+  wealth <- f[t,xx-x_crit+1]
+}
+# fitness <- function(x,t)
+# {
+#   xx <- pmin(x ,x_max)
+#   xx <- pmax(xx,x_crit)
+#   fitness <- f[t,xx-x_crit+1]
+# }
+
+# inherits()
